e63213913d75b9b0e88396cb0e21215f6414d630
migno_chrono <Carlos.BarrosoPenaranda@my.strayer.edu>
2022-07-04 21:09:02 -0400

    first commit

---
+++ scripts/myscripts.js
@@ -0,0 +1,10 @@
+var button = document.querySelector('button');
+var box = document.getElementById('changeMe');
+
+function changeColor (){
+    if(box.style.background == 'red'){
+        box.style.background = 'blue';
+    }else{
+box.style.background = 'red'
+    }
+}
